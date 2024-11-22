107c7555ee676167b2b89f4d75be3fd315c7cd1a
萧无曦 <60769754+Wuxi-Xiao@users.noreply.github.com>
2024-11-22 11:28:57 +1100

    Add files via upload
    Update current progress

---
+++ Newsticker.js
@@ -0,0 +1,12 @@
+const newsTicker = document.querySelector('.news-ticker ul');
+const newsItems = document.querySelectorAll('.news-ticker li');
+const newsItemWidth = newsItems[0].offsetWidth;
+const newsItemCount = newsItems.length;
+let currentIndex = 0;
+
+function scrollNews() {
+  currentIndex = (currentIndex + 1) % newsItemCount;
+  newsTicker.style.transform = `translateX(-${currentIndex * newsItemWidth}px)`;
+}
+
+setInterval(scrollNews, 10000); // 每2秒滚动一次
