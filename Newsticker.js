13c2c529b15a36fb71a05994703a66d2c30757bf
Jack Wang <60769754+Wuxi-Xiao@users.noreply.github.com>
2024-11-27 02:35:27 +1100

    Update 27/11/2024

--- Newsticker.js
+++ Newsticker.js
@@ -1,11 +1,15 @@
 const newsTicker = document.querySelector('.news-ticker ul');
-const newsItems = document.querySelectorAll('.news-ticker li');
-const newsItemWidth = newsItems[0].offsetWidth;
-const newsItemCount = newsItems.length;
+const newsItems = document.querySelectorAll('.news-ticker li'); // 获取所有新闻项 // Get all news
+const newsItemWidth = newsItems[0].offsetWidth; // 单个宽度 // Single width
+const newsItemCount = newsItems.length; // 新闻总数 // Total number of news
 let currentIndex = 0;
 
 function scrollNews() {
+  // 更新当前新闻项的索引，确保在新闻项总数范围内循环
+  // Update the current news item index to ensure it loops within the total number of news items
   currentIndex = (currentIndex + 1) % newsItemCount;
+  // 更新新闻滚动条的transform属性，实现滚动效果
+  // Update the transform property of the news ticker to achieve the scrolling effect
   newsTicker.style.transform = `translateX(-${currentIndex * newsItemWidth}px)`;
 }
 
--- sw.js
+++ sw.js
@@ -1,6 +1,4 @@
-// 定义缓存名称为 "temperature-converter-v1" 
-// Define cache name as "temperature-converter-v1"
-const CACHE_NAME = `temperature-converter-v1`; 
+const CACHE_NAME = `Game-Tracker-Beta`; // 定义缓存名称为 "Game-Tracker-Beta"  // Define cache name as "Game-Tracker-Beta `"
 
 // 使用 install 事件预缓存所有初始资源
 // Use the install event to pre-cache all initial resources
@@ -9,8 +7,8 @@ self.addEventListener('install', event => {
     const cache = await caches.open(CACHE_NAME); // 打开缓存 // Open the cache
     cache.addAll([ // 将以下资源添加到缓存 // Add the following resources to the cache
       '/',
-      '/converter.js',
-      '/converter.css'
+      'Stylesheet/Newsticker.js',
+      'Stylesheet/Stylesheet.css'
     ]);
   })());
 });
@@ -20,7 +18,6 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith((async () => {
     const cache = await caches.open(CACHE_NAME); // 打开缓存 // Open the cache
-
     // 从缓存中获取资源
     // Get the resource from the cache
     const cachedResponse = await cache.match(event.request);
@@ -31,10 +28,13 @@ self.addEventListener('fetch', event => {
         // 如果缓存中没有资源，尝试从网络获取
         // If the resource was not in the cache, try the network
         const fetchResponse = await fetch(event.request);
-
-        // 将资源保存到缓存并返回
-        // Save the resource in the cache and return it
-        cache.put(event.request, fetchResponse.clone());
+         // 确保只缓存GET或HEAD请求
+        // Ensure that only GET or HEAD requests are cached
+        if (event.request.method === 'GET' || event.request.method === 'HEAD') {
+          // 将资源保存到缓存并返回
+          // Save the resource in the cache and return it
+          cache.put(event.request, fetchResponse.clone());
+        }
         return fetchResponse;
       } catch (e) {
         // 网络请求失败
